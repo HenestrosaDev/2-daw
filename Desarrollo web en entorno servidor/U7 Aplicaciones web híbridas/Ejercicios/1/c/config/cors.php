1bb008ccf1125a81455f7968588f149c6c511fa7
HenestrosaDev <henestrosadev@gmail.com>
2024-06-23 15:24:39 +0200

    Cambia la indentación de U7/1c de DWEC a tabs y arregla el código para que funcione correctamente

--- Desarrollo web en entorno servidor/U7 Aplicaciones web híbridas/Ejercicios/1/c/config/cors.php
+++ Desarrollo web en entorno servidor/U7 Aplicaciones web híbridas/Ejercicios/1/c/config/cors.php
@@ -2,33 +2,33 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Cross-Origin Resource Sharing (CORS) Configuration
+	|--------------------------------------------------------------------------
+	|
+	| Here you may configure your settings for cross-origin resource sharing
+	| or "CORS". This determines what cross-origin operations may execute
+	| in web browsers. You are free to adjust these settings as needed.
+	|
+	| To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+	|
+	*/
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+	'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+	'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+	'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+	'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+	'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+	'exposed_headers' => [],
 
-    'max_age' => 0,
+	'max_age' => 0,
 
-    'supports_credentials' => false,
+	'supports_credentials' => false,
 
 ];
--- Desarrollo web en entorno servidor/U7 Aplicaciones web híbridas/Ejercicios/1/c/database/factories/UserFactory.php
+++ Desarrollo web en entorno servidor/U7 Aplicaciones web híbridas/Ejercicios/1/c/database/factories/UserFactory.php
@@ -10,33 +10,33 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition()
+	{
+		return [
+			'name' => $this->faker->name(),
+			'email' => $this->faker->unique()->safeEmail(),
+			'email_verified_at' => now(),
+			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+			'remember_token' => Str::random(10),
+		];
+	}
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
+	/**
+	 * Indicate that the model's email address should be unverified.
+	 *
+	 * @return static
+	 */
+	public function unverified()
+	{
+		return $this->state(function (array $attributes) {
+			return [
+				'email_verified_at' => null,
+			];
+		});
+	}
 }
