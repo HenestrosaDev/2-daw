07698b626855b016ebd43817f36adb3e7d76adb2
HenestrosaDev <henestrosadev@gmail.com>
2024-06-26 10:56:53 +0200

    Convierte la columna `role` en tipo `enum`

--- Desarrollo web en entorno servidor/U7 Aplicaciones web híbridas/Ejercicios/7/database/migrations/0001_01_01_000000_create_users_table.php
+++ Desarrollo web en entorno servidor/U7 Aplicaciones web híbridas/Ejercicios/7/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
 			$table->string('email')->unique();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
-						$table->string('role')->default('client');
+			$table->enum('role', ['client', 'admin'])->default('client');
 			$table->rememberToken();
 			$table->timestamps();
 		});
