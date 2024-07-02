4d6e32534892ee9290616f1fb7815b4a71e53570
HenestrosaDev <henestrosadev@gmail.com>
2024-07-02 15:57:22 +0200

    Formatea archivos de la tarea de la U6 de DIW

--- Diseño de interfaces web/U6 Contenidos web interactivos/Ejercicios/js/canvas.js
+++ Diseño de interfaces web/U6 Contenidos web interactivos/Ejercicios/js/canvas.js
@@ -2,63 +2,63 @@
  * Cargamos los botones renderizados con la API Canvas
  */
 export default function setUpButtons() {
-  // Botón cargar
-  const load = $("#video-load");
-  const loadContext = load[0].getContext("2d");
+	// Botón cargar
+	const load = $("#video-load");
+	const loadContext = load[0].getContext("2d");
 
-  // Triángulo de arriba
-  loadContext.beginPath();
-  loadContext.fillStyle = "red";
-  loadContext.moveTo(5, 20);
-  loadContext.lineTo(20, 5);
-  loadContext.lineTo(35, 20);
-  loadContext.closePath();
-  loadContext.fill();
+	// Triángulo de arriba
+	loadContext.beginPath();
+	loadContext.fillStyle = "red";
+	loadContext.moveTo(5, 20);
+	loadContext.lineTo(20, 5);
+	loadContext.lineTo(35, 20);
+	loadContext.closePath();
+	loadContext.fill();
 
-  // Rectángulo de abajo
-  loadContext.beginPath();
-  loadContext.fillStyle = "red";
-  loadContext.moveTo(5, 25);
-  loadContext.lineTo(35, 25);
-  loadContext.lineTo(35, 33);
-  loadContext.lineTo(5, 33);
-  loadContext.closePath();
-  loadContext.fill();
+	// Rectángulo de abajo
+	loadContext.beginPath();
+	loadContext.fillStyle = "red";
+	loadContext.moveTo(5, 25);
+	loadContext.lineTo(35, 25);
+	loadContext.lineTo(35, 33);
+	loadContext.lineTo(5, 33);
+	loadContext.closePath();
+	loadContext.fill();
 
-  // Botón reproducir
-  const play = $("#video-play");
-  const playContext = play[0].getContext("2d");
+	// Botón reproducir
+	const play = $("#video-play");
+	const playContext = play[0].getContext("2d");
 
-  // Triángulo negro
-  playContext.beginPath();
-  playContext.fillStyle = "green";
-  playContext.moveTo(5, 5);
-  playContext.lineTo(35, 20);
-  playContext.lineTo(5, 35);
-  playContext.closePath();
-  playContext.fill();
+	// Triángulo negro
+	playContext.beginPath();
+	playContext.fillStyle = "green";
+	playContext.moveTo(5, 5);
+	playContext.lineTo(35, 20);
+	playContext.lineTo(5, 35);
+	playContext.closePath();
+	playContext.fill();
 
-  // Botón pausa
-  const pause = $("#video-pause");
-  const pauseContext = pause[0].getContext("2d");
+	// Botón pausa
+	const pause = $("#video-pause");
+	const pauseContext = pause[0].getContext("2d");
 
-  // Rectángulo izquierdo
-  pauseContext.beginPath();
-  pauseContext.fillStyle = "black";
-  pauseContext.moveTo(5, 5);
-  pauseContext.lineTo(15, 5);
-  pauseContext.lineTo(15, 35);
-  pauseContext.lineTo(5, 35);
-  pauseContext.closePath();
-  pauseContext.fill();
+	// Rectángulo izquierdo
+	pauseContext.beginPath();
+	pauseContext.fillStyle = "black";
+	pauseContext.moveTo(5, 5);
+	pauseContext.lineTo(15, 5);
+	pauseContext.lineTo(15, 35);
+	pauseContext.lineTo(5, 35);
+	pauseContext.closePath();
+	pauseContext.fill();
 
-  // Rectángulo derecho
-  pauseContext.beginPath();
-  pauseContext.fillStyle = "black";
-  pauseContext.moveTo(25, 5);
-  pauseContext.lineTo(35, 5);
-  pauseContext.lineTo(35, 35);
-  pauseContext.lineTo(25, 35);
-  pauseContext.closePath();
-  pauseContext.fill();
-};
+	// Rectángulo derecho
+	pauseContext.beginPath();
+	pauseContext.fillStyle = "black";
+	pauseContext.moveTo(25, 5);
+	pauseContext.lineTo(35, 5);
+	pauseContext.lineTo(35, 35);
+	pauseContext.lineTo(25, 35);
+	pauseContext.closePath();
+	pauseContext.fill();
+}
--- Diseño de interfaces web/U6 Contenidos web interactivos/Ejercicios/js/tarea_06.js
+++ Diseño de interfaces web/U6 Contenidos web interactivos/Ejercicios/js/tarea_06.js
@@ -4,103 +4,102 @@
 import setUpButtons from "./canvas.js";
 
 /**
- * Cargamos las funciones que cargan los eventos del navbar, del footer y 
+ * Cargamos las funciones que cargan los eventos del navbar, del footer y
  * de los bloques respectivos a la imagen, al vídeo y a la animación cuando
  * el documento cargue para.
  */
 $(function () {
 	handleNavbarOptions();
 	handleTextFooterHover();
-	
+
 	setUpImageBlock();
 	setUpVideoBlock();
-  setUpAnimationBlock();
+	setUpAnimationBlock();
 });
 
 /**
  * Maneja las opciones del navbar.
  */
 const handleNavbarOptions = () => {
-  // Elementos del DOM
-  const image = $("#navbar-image");
-  const video = $("#navbar-video");
-  const animation = $("#navbar-animation");
+	// Elementos del DOM
+	const image = $("#navbar-image");
+	const video = $("#navbar-video");
+	const animation = $("#navbar-animation");
 
-  const elements = [image, video, animation];
-  const classes = ["fs--md", "text--blue"];
+	const elements = [image, video, animation];
+	const classes = ["fs--md", "text--blue"];
 
-  // Se maneja el evento click sobre el botón Imagen
-  image.on("click", () => {
-    handleNavbarClasses(classes, elements, image);
-    handleHiddenSections("-image");
-  });
+	// Se maneja el evento click sobre el botón Imagen
+	image.on("click", () => {
+		handleNavbarClasses(classes, elements, image);
+		handleHiddenSections("-image");
+	});
 
-  // Se maneja el evento click sobre el botón Vídeo
-  video.on("click", () => {
-    handleNavbarClasses(classes, elements, video);
-    handleHiddenSections("-video");
-  });
+	// Se maneja el evento click sobre el botón Vídeo
+	video.on("click", () => {
+		handleNavbarClasses(classes, elements, video);
+		handleHiddenSections("-video");
+	});
 
-  // Se maneja el evento click sobre el botón Animación
-  animation.on("click", () => {
-    handleNavbarClasses(classes, elements, animation);
-    handleHiddenSections("-animation");
-  });
-}
+	// Se maneja el evento click sobre el botón Animación
+	animation.on("click", () => {
+		handleNavbarClasses(classes, elements, animation);
+		handleHiddenSections("-animation");
+	});
+};
 
 /**
  * Se encarga de cambiar la fuente y el color de la letra de la opción
  * seleccionada
  *
  * @param {Array} classes	Clases para añadir/eliminar de un elemento
- * @param {Array} elements	Elementos a los que se le quitan las clases 
+ * @param {Array} elements	Elementos a los que se le quitan las clases
  * @param {Element} selectedElement	Elemento al que se le añaden las clases
  */
 const handleNavbarClasses = (classes, elements, selectedElement) => {
-  classes.forEach((c) => {
-    elements.map((element) => {
-      if (element.is(selectedElement)) {
-        element.addClass(c);
-      } else {
-        element.removeClass(c);
-      }
-    });
-  });
+	classes.forEach((c) => {
+		elements.map((element) => {
+			if (element.is(selectedElement)) {
+				element.addClass(c);
+			} else {
+				element.removeClass(c);
+			}
+		});
+	});
 };
 
 /**
  * Muestra y oculta las secciones relacionadas con la opción seleccionada en el
  * navbar
  *
- * @param {Element} toShow 
+ * @param {Element} toShow
  */
 const handleHiddenSections = (toShow) => {
-  // Oculta los elementos que se cargan cuando no hay ninguna opción
-  // seleccionada
-  $("[id$='-to-hide']").addClass("hidden", "fade-out");
- 
+	// Oculta los elementos que se cargan cuando no hay ninguna opción
+	// seleccionada
+	$("[id$='-to-hide']").addClass("hidden", "fade-out");
 
-  // Oculta el bloque deseleccionado
-  const contentNodes = document.querySelectorAll("[id$='-content']");
-  const contentArray = [...contentNodes];
+	// Oculta el bloque deseleccionado
+	const contentNodes = document.querySelectorAll("[id$='-content']");
+	const contentArray = [...contentNodes];
 
-  contentArray.forEach((element) => {
-    for (let i = 0; i < element.children.length; i++) {
-      const child = element.children[i];
-      if (!child.classList.contains("hidden")) {
-        child.classList.add("hidden");
-      }
-    }
-  });
+	contentArray.forEach((element) => {
+		for (let i = 0; i < element.children.length; i++) {
+			const child = element.children[i];
+			if (!child.classList.contains("hidden")) {
+				child.classList.add("hidden");
+			}
+		}
+	});
 
-  // Muestra el bloque seleccionado
-  $(`[id$='${toShow}']`).each(function() {
-    if ($(this).hasClass("hidden")) {
-      $(this).removeClass("hidden");
-      $(this).addClass("fade-in");
-    }
-  });
-}
+	// Muestra el bloque seleccionado
+	$(`[id$='${toShow}']`).each(function () {
+		if ($(this).hasClass("hidden")) {
+			$(this).removeClass("hidden");
+			$(this).addClass("fade-in");
+		}
+	});
+};
 
 /**
  * Cuando hacemos hover sobre el texto del footer, esta opción se encargará de
@@ -108,21 +107,21 @@ const handleHiddenSections = (toShow) => {
  */
 const handleTextFooterHover = () => {
 	$("#footer-text").hover(
-    // on hover
-    function () {
-      $(this).css("color", "white");
-      $("footer").css({
+		// on hover
+		function () {
+			$(this).css("color", "white");
+			$("footer").css({
 				"background-color": "black",
-				"transition": "background-color 1s ease-out"
+				transition: "background-color 1s ease-out",
 			});
-    },
-    // on mouseout
-    function () {
-      $(this).css("color", "");
-      $("footer").css("background-color", "");
-    }
-  );
-}
+		},
+		// on mouseout
+		function () {
+			$(this).css("color", "");
+			$("footer").css("background-color", "");
+		}
+	);
+};
 
 /*
  * IMAGE BLOCK
@@ -133,7 +132,7 @@ const handleTextFooterHover = () => {
  * Carga el bloque de las imágenes
  */
 const setUpImageBlock = () => {
-	$('input[name="distribution"]').on("change", function() {
+	$('input[name="distribution"]').on("change", function () {
 		if ($(this).val() === "1") {
 			toggleDistributions("#distribution-2", "#distribution-1");
 		} else {
@@ -142,47 +141,49 @@ const setUpImageBlock = () => {
 	});
 
 	$("[class^=distribution__img--]").on("click", function () {
-    const borderClass = "bordered--red";
+		const borderClass = "bordered--red";
 		if ($(this).hasClass(borderClass)) {
-			$(this).removeClass(borderClass)
+			$(this).removeClass(borderClass);
 		} else {
 			$(this).addClass(borderClass);
 		}
-  });
+	});
 
 	$("#image-filter-apply").on("click", function () {
-    // Obtenemos el filtro a aplicar
-    const filterToApply = `
+		// Obtenemos el filtro a aplicar
+		const filterToApply = `
 			${$("#image-filter-name").val()}(
 				${$("#image-filter-value").val()}%
 			)
 		`;
 
-    // Obtenemos las imágenes seleccionadas
+		// Obtenemos las imágenes seleccionadas
 		let imagesToFilter = $(".bordered--red")
-      .map(function () { return this.id })
-      .get();
+			.map(function () {
+				return this.id;
+			})
+			.get();
 
 		imagesToFilter = new Intl.ListFormat("es", { type: "conjunction" }).format(
-      imagesToFilter
-    ); 
+			imagesToFilter
+		);
 
-    // Añadimos el filtro al historial
+		// Añadimos el filtro al historial
 		addFilterToHistory(filterToApply, imagesToFilter);
 
-    // Quitamos el borde rojo y aplicamos el filtro a la imagen seleccionada
-    $(".bordered--red")
-    	.removeClass("bordered--red")
-      .css("filter", filterToApply);
+		// Quitamos el borde rojo y aplicamos el filtro a la imagen seleccionada
+		$(".bordered--red")
+			.removeClass("bordered--red")
+			.css("filter", filterToApply);
 
-      // Reestablecemos el valor del <select>
+		// Reestablecemos el valor del <select>
 		$("#image-filter-name").val("default");
-  });
-}
+	});
+};
 
 /**
  * Maneja el cambio de distribuciones
- * 
+ *
  * @param {Element} toHide  Bloque para ocultar
  * @param {*} toShow  Bloque para mostrar
  */
@@ -190,16 +191,14 @@ const toggleDistributions = (toHide, toShow) => {
 	$(toHide).slideUp("slow", function () {
 		$("#image-filter-history").empty();
 
-    $(toShow).slideDown("slow");
-    $(`${toHide} > *`)
-      .removeClass("bordered--red")
-      .removeAttr("style"); // elimina los filtros
-  });
-}
+		$(toShow).slideDown("slow");
+		$(`${toHide} > *`).removeClass("bordered--red").removeAttr("style"); // elimina los filtros
+	});
+};
 
 /**
  * Añade el filtro renderizado al historial de filtros
- * 
+ *
  * @param {String} filter  Filtro renderizado
  * @param {String} elements Elementos con el filtro aplicado
  */
@@ -207,7 +206,7 @@ const addFilterToHistory = (filter, elements) => {
 	if (!filter.includes("default")) {
 		$("#image-filter-history").prepend(`<p>${filter} | ${elements}</p>`);
 	}
-}
+};
 
 /*
  * VIDEO BLOCK
@@ -218,39 +217,39 @@ const addFilterToHistory = (filter, elements) => {
  * Carga el bloque de los vídeos
  */
 const setUpVideoBlock = () => {
-  let selectedVideo;
+	let selectedVideo;
 
-  $('input[name="video"]').on("change", function () {
-    selectedVideo = $(this).val();
-  });
+	$('input[name="video"]').on("change", function () {
+		selectedVideo = $(this).val();
+	});
 
-  // Mostramos el vídeo cuando pulsamos el botón cargar
-  $("#video-load").on("click", function () {
-    if (selectedVideo === "1") {
-      $("#video-1").removeClass("hidden");
-      $("#video-2").addClass("hidden");
-      $("#video-2")[0].pause();
-    } else if (selectedVideo === "2") {
-      $("#video-1").addClass("hidden");
-      $("#video-1")[0].pause();
-      $("#video-2").removeClass("hidden");
-    }
-    $(`#video-${selectedVideo}`).get(0).currentTime = 0;
-  });
+	// Mostramos el vídeo cuando pulsamos el botón cargar
+	$("#video-load").on("click", function () {
+		if (selectedVideo === "1") {
+			$("#video-1").removeClass("hidden");
+			$("#video-2").addClass("hidden");
+			$("#video-2")[0].pause();
+		} else if (selectedVideo === "2") {
+			$("#video-1").addClass("hidden");
+			$("#video-1")[0].pause();
+			$("#video-2").removeClass("hidden");
+		}
+		$(`#video-${selectedVideo}`).get(0).currentTime = 0;
+	});
 
-  // Reproducimos el vídeo cuando pulsamos el play
-  $("#video-play").on("click", function () {
-    $(`#video-${selectedVideo}`).get(0).play();
-  });
+	// Reproducimos el vídeo cuando pulsamos el play
+	$("#video-play").on("click", function () {
+		$(`#video-${selectedVideo}`).get(0).play();
+	});
 
-  // Pausamos el vídeo cuando pulsamos sobre pause 
-  $("#video-pause").on("click", function () {
-    $(`#video-${selectedVideo}`).get(0).pause();
-  });
+	// Pausamos el vídeo cuando pulsamos sobre pause
+	$("#video-pause").on("click", function () {
+		$(`#video-${selectedVideo}`).get(0).pause();
+	});
 
-  // Función importada del fichero canvas.js para cargar los botones
-  setUpButtons();
-}
+	// Función importada del fichero canvas.js para cargar los botones
+	setUpButtons();
+};
 
 /*
  * ANIMATION BLOCK
@@ -258,16 +257,16 @@ const setUpVideoBlock = () => {
  */
 
 const setUpAnimationBlock = () => {
-  // Cambiamos la duración de la animación en cuanto el usuario cambie su valor
-  $("#animation-length").on("change", function () {
-    $("#animation-canvas").css(
-      "animation",
-      `square-translation ${$(this).val()}s linear infinite`
-    );
-  });
+	// Cambiamos la duración de la animación en cuanto el usuario cambie su valor
+	$("#animation-length").on("change", function () {
+		$("#animation-canvas").css(
+			"animation",
+			`square-translation ${$(this).val()}s linear infinite`
+		);
+	});
 
-  // Cambiamos la curvatura en cuanto el usuario cambie su valor
-  $("#animation-radius").on("change", function () {
-    $("#animation-canvas").css("border-radius", `${$(this).val()}px`);
-  });
-}
+	// Cambiamos la curvatura en cuanto el usuario cambie su valor
+	$("#animation-radius").on("change", function () {
+		$("#animation-canvas").css("border-radius", `${$(this).val()}px`);
+	});
+};
